d6342ac61227ae0b7357b3a16a88d9916d69568c
Ken Schwartz <ken@kennethmschwartz.com>
2015-11-04 08:59:43 -0500

    Final check in. Cleaning up.

--- Competition/train.R
+++ Competition/train.R
@@ -1,3 +1,4 @@
+# setwd("Z:/The Analytics Edge/Competition")
 # to read: source("train.R")
 
 library(rpart)
@@ -5,7 +6,6 @@ library(rpart.plot)
 library(caTools)
 library(tm)
 
-setwd("Z:/The Analytics Edge/Competition")
 ebay = read.csv("eBayiPadTrain.csv", stringsAsFactors=FALSE)
 ebay$sold = as.factor(ebay$sold)
 ebay$biddable = as.factor(ebay$biddable)
@@ -15,18 +15,28 @@ ebay$carrier = as.factor(ebay$carrier)
 ebay$color = as.factor(ebay$color)
 ebay$storage = as.factor(ebay$storage)
 ebay$productline = as.factor(ebay$productline)
+# table(ebay$sold,log(ebay$startprice) > 6.515)
+ebay$tooExpensive = log(ebay$startprice) > 6.515
+table(ebay$sold, ebay$tooExpensive)
+
 #ebay$notWorking = ebay$condition == "For parts or not working"
 #ebay$unknown = ebay$productline == "Unknown"
 
 
 # split the data
+#biddable = subset(ebay, biddable == 1 & tooExpensive == FALSE)
 biddable = subset(ebay, biddable == 1)
 biddable$biddable = NULL
 biddable$UniqueID = NULL
+#notBiddable = subset(ebay, biddable == 0 & tooExpensive == FALSE)
 notBiddable = subset(ebay, biddable == 0)
 notBiddable$biddable = NULL
 notBiddable$UniqueID = NULL
 
+#notBiddableExpensive = subset(ebay, biddable == 0 & log(startprice) > 5.9)
+#notBiddableExpensive$biddable = NULL
+#notBiddableExpensive$UniqueID = NULL
+#notBiddableExpensive$startprice = log(notBiddableExpensive$startprice)
 
 # Create corpus
 corpus = Corpus(VectorSource(biddable$description))
@@ -85,12 +95,67 @@ biddable$productlineiPadminiRetina = biddable$productline == "iPad mini Retina"
 biddable$productlineUnknown = biddable$productline == "Unknown"
 biddable$productline = NULL
 
+corpus2 = Corpus(VectorSource(notBiddable$description))
+corpus2[[1]]
+corpus2 = tm_map(corpus2, tolower)
+corpus2 = tm_map(corpus2, PlainTextDocument)
+corpus2 = tm_map(corpus2, removePunctuation)
+corpus2 = tm_map(corpus2, removeWords, stopwords("english"))
+corpus2 = tm_map(corpus2, stemDocument)
+corpus2[[1]]
+
+dtm2 = DocumentTermMatrix(corpus2)
+dtm2
+
+# Remove sparse terms
+dtm2x = removeSparseTerms(dtm2, 0.92)
+dtm2x
+
+
+labeledTerms2 = as.data.frame(as.matrix(dtm2x))
+colnames(labeledTerms2) = make.names(colnames(labeledTerms2))
+# Add in the outcome variable
+labeledTerms2$sold = notBiddable$sold
+
+notBiddable$condit=labeledTerms2$condit
+notBiddable$good=labeledTerms2$good
+notBiddable$ipad=labeledTerms2$ipad
+notBiddable$scratch=labeledTerms2$scratch
+notBiddable$use=labeledTerms2$use
+notBiddable$work=labeledTerms2$work
+notBiddable$description = NULL
+
+notBiddable$productlineiPad1 = notBiddable$productline == "iPad 1"
+notBiddable$productlineiPad2 = notBiddable$productline == "iPad 2"
+notBiddable$productlineiPad3 = notBiddable$productline == "iPad 3"
+notBiddable$productlineiPad4 = notBiddable$productline == "iPad 4"
+notBiddable$productlineiPad5 = notBiddable$productline == "iPad 5"
+notBiddable$productlineiPadAir = notBiddable$productline == "iPad Air"
+notBiddable$productlineiPadAir2 = notBiddable$productline == "iPad Air 2"
+notBiddable$productlineiPadmini = notBiddable$productline == "iPad mini"
+notBiddable$productlineiPadmini2 = notBiddable$productline == "iPad mini 2"
+notBiddable$productlineiPadmini3 = notBiddable$productline == "iPad mini 3"
+notBiddable$productlineiPadminiRetina = notBiddable$productline == "iPad mini Retina"
+notBiddable$productlineUnknown = notBiddable$productline == "Unknown"
+notBiddable$productline = NULL
+
+notBiddable$startprice = log(notBiddable$startprice)
+
 set.seed(47308)
 spl = sample.split(biddable$sold, 0.7)
 biddableTrain = subset(biddable, spl == TRUE)
 biddableTest = subset(biddable, spl == FALSE)
 biddableTrain$productline = NULL
 biddableTest$productline = NULL
+
+set.seed(47308)
+spl = sample.split(notBiddable$sold, 0.7)
+notBiddableTrain = subset(notBiddable, spl == TRUE)
+notBiddableTest = subset(notBiddable, spl == FALSE)
+notBiddableTrain$productline = NULL
+notBiddableTest$productline = NULL
+
+
 
 LogModel = glm(sold ~ ., data=biddable, family="binomial")
 summary(LogModel)
@@ -100,7 +165,7 @@ table(biddable$sold, predictLog > 0.5)
 # 0.8685783
 
 
-LogModel2 = glm(sold ~ startprice + condition + storage + crack + great + scratch + use + productlineiPad1 + productlineiPadAir + productlineiPadAir2 + productlineiPadmini2 + productlineiPadmini3, data=biddable, family="binomial")
+LogModel2 = glm(sold ~ startprice + tooExpensive + condition + storage + crack + great + scratch + use + productlineiPad1 + productlineiPadAir + productlineiPadAir2 + productlineiPadmini2 + productlineiPadmini3, data=biddable, family="binomial")
 summary(LogModel2)
 predictLog2 = predict(LogModel2, type="response")
 table(biddable$sold, predictLog2 > 0.5)
@@ -125,21 +190,72 @@ table(biddableTest$sold, predictTest > 0.5)
 
 
 # ------ MODEL 2 BEGIN ------
+#biddableTrain$startprice = log(biddableTrain$startprice)
+#biddableTest$startprice = log(biddableTest$startprice)
 LogModel2Train = glm(sold ~ startprice + condition + storage + crack + great + scratch + use + productlineiPad1 + productlineiPadAir + productlineiPadAir2 + productlineiPadmini2 + productlineiPadmini3, data=biddableTrain, family="binomial")
 #summary(LogModel2Train)
 predictLog2Train = predict(LogModel2Train, type="response")
 table(biddableTrain$sold, predictLog2Train > 0.5)
-(63+429)/(63+429+75+19)
-# 0.8395904
+(66+430)/(66+430+68+18)
+# 0.8522337
 
 # Predictions on the test set
 predictTest2 = predict(LogModel2Train, type="response", newdata=biddableTest)
 table(biddableTest$sold, predictTest2 > 0.5)
-(33+183)/(33+183+26+9)
+(31+181)/(31+181+27+11)
 # [1] 0.8605578
 # ------ MODEL 2 END ------
 
 
+# --------------------- NOT BIDDABLE BEGIN
+
+# ------ MODEL 1 BEGIN ------
+#notBiddableTrain$startprice = log(notBiddableTrain$startprice)
+#notBiddableTest$startprice = log(notBiddableTest$startprice)
+LogModelTrain2 = glm(sold ~ ., data=notBiddableTrain, family="binomial")
+summary(LogModelTrain2)
+predictLogTrain2 = predict(LogModelTrain2, type="response")
+table(notBiddableTrain$sold, predictLogTrain2 > 0.5)
+(559+6)/(559+6+4+148)
+# 0.7880056
+
+# at least this one uses price as the biggie
+#LogModelTrain2 = glm(sold ~ startprice + storage + hous + item + sign
+#+ productlineiPadmini2,  data=notBiddableTrain, family="binomial")
+#summary(LogModelTrain2)
+
+LogModelTrain2 = glm(sold ~ startprice + scratch + work,  data=notBiddableTrain, family="binomial")
+summary(LogModelTrain2)
+predictLogTrain2 = predict(LogModelTrain2, type="response")
+table(notBiddableTrain$sold, predictLogTrain2 > 0.5)
+(559+2)/(554+2+4+152)
+# 0.7879213
+
+LogModelTrain2 = glm(sold ~ startprice + condition + scratch + work,  data=notBiddableTrain, family="binomial")
+summary(LogModelTrain2)
+predictLogTrain2 = predict(LogModelTrain2, type="response")
+table(notBiddableTrain$sold, predictLogTrain2 > 0.5)
+(561+2)/(561+2+2+152)
+# 0.7852162
+
+# Predictions on the test set
+predictTest2 = predict(LogModelTrain2, type="response", newdata=notBiddableTest)
+table(notBiddableTest$sold, predictTest2 > 0.5)
+(28+179)/(28+179+31+13)
+# [1] 0.8247012
+
+notBiddableCART = rpart(sold ~ startprice + condition, data=notBiddableTrain, method="class", cp=0.0)
+prp(notBiddableCART, tweak=1.5, gap=0, space=0)
+pred = predict(notBiddableCART, newdata=notBiddableTest)
+pred[1:10,]
+pred.prob = pred[,2]
+table(notBiddableTest$sold, pred.prob >= 0.5)
+(226+9)/(226+9+15+57)
+# [1] 0.7654723
+
+# ------ MODEL 1 END ------
+
+# --------------------- NOT BIDDABLE END
 
 
 # cart
